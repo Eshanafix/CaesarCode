f4c55de7c8783d0e6f2207a6ced953258d92a11c
admin <admin@10.3.93.208>
2018-11-15 13:19:46 -0600

    Ok u

--- Ciphers/src/cipherSplosion/CaesarCipherDecoder.java
+++ Ciphers/src/cipherSplosion/CaesarCipherDecoder.java
@@ -1,6 +1,22 @@
 package cipherSplosion;
 
+import java.io.*;
+
 public class CaesarCipherDecoder {
-		//Working github
-	int happy = 0;
+	public static void main(String[] args) {
+		
+	}
+	
+	public boolean checkIfWord(String input) {
+		
+		String[] check = input.split(" ");
+		
+		try(BufferedReader bReader = new BufferedReader(new FileReader("wordList.txt"))){
+			
+		}
+		catch(IOException e) {
+			
+		}
+	}
+	
 }
